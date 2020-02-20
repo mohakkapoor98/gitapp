8300d4d1dee6d4f6bb79c6b919df4bc72b091816
Mohak <mohkap98@gmail.com>
2020-02-20 16:31:43 -0500

    first commit

---
+++ myApp/Controllers/storeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace myApp.Controllers
+{
+    public class storeController : Controller
+    {
+        // GET: store
+        public string Index()
+        {
+            return "yo";
+        }
+        public string brownse(string genre)
+        {
+            string message = 
+                HttpUtility.HtmlEncode("Store.Browse, Genre = " + genre); 
+            return message;
+        }
+        public string fetch()
+        {
+            return "yo from 2";
+        }
+
+    }
+}
